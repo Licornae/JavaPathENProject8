1490a48426ace08481d666d627f19a046213db3f
Caroline <bergere.caroline@outlook.fr>
2026-06-30 23:58:58 +0200

    Remove `@Disabled` annotations from test methods in `TestPerformance` and `TestRewardsService`.

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -44,8 +44,6 @@ public class TestPerformance {
 	 * assertTrue(TimeUnit.MINUTES.toSeconds(20) >=
 	 * TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	 */
-
-	@Disabled
 	@Test
 	public void highVolumeTrackLocation() {
 		GpsUtil gpsUtil = new GpsUtil();
